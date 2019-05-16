dbe2424084865d2641cf0ef132dc08c16b066525
Francesca Chater <francescachater@gmail.com>
2019-05-16 14:22:25 +0100

    Feature test passed to see player 2 HP

--- spec/features/view_hitpoints_spec.rb
+++ spec/features/view_hitpoints_spec.rb
@@ -1,9 +1,9 @@
-feature 'Enter names' do
-  scenario 'submitting names' do
+feature 'View hitpoints' do
+  scenario 'see player 2\'s hitpoints' do
     visit('/')
     fill_in 'player_1_name', with: 'Hercules'
     fill_in 'player_2_name', with: 'Hades'
     click_button 'Submit'
-    expect(page).to have_content 'Hercules vs. Hades'
+    expect(page).to have_content 'Hades: 60HP'
   end
 end
